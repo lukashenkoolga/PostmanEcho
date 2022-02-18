353ef0ca6b0b98077076762cd05ab8eac3d3ff2d
Olga Lukashenko <lukashenko.93@bk.ru>
2022-02-18 23:56:25 +0300

    Build failed

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,7 +20,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some dat"))
         ;
     }
 }
